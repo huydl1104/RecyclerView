377a2875f5bfa5a1b97f78e17b01d6d9068ff7c3
余栋梁 <yudongliang@okay.cn>
2021-10-12 10:33:28 +0800

    aop 配置修改

---
+++ app/src/main/java/com/ydl/list/aop/TestClick.java
@@ -0,0 +1,10 @@
+package com.ydl.list.aop;
+import org.aspectj.lang.annotation.Aspect;
+/**
+ * @author yudongliang
+ * create time 2021-10-12
+ * describe :
+ */
+@Aspect
+public class TestClick {
+}
