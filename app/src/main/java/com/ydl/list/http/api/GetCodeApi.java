64ac8546f08004da069a9eff1e827a64acdbb00d
余栋梁 <yudongliang@okay.cn>
2021-10-13 14:33:02 +0800

    移植dialog 和 http 模块

---
+++ app/src/main/java/com/ydl/list/http/api/GetCodeApi.java
@@ -0,0 +1,22 @@
+package com.ydl.list.http.api;
+
+import com.hjq.http.config.IRequestApi;
+
+/**
+ * 获取验证码
+ */
+public final class GetCodeApi implements IRequestApi {
+
+    @Override
+    public String getApi() {
+        return "code/get";
+    }
+
+    /** 手机号 */
+    private String phone;
+
+    public GetCodeApi setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+}
